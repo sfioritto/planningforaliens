ec41bcee115f7148455272e6c8608f6260776f4b
Sean Fioritto <sean@planningforaliens.com>
2024-03-16 15:25:16 -0500

    Add published date

--- src/routes/blog/layout.tsx
+++ src/routes/blog/layout.tsx
@@ -2,11 +2,12 @@ import { component$, Slot } from "@builder.io/qwik";
 import { useDocumentHead } from "@builder.io/qwik-city";
 
 export default component$(() => {
-  const { title } = useDocumentHead();
+  const { title, frontmatter } = useDocumentHead();
   return (
     <>
       <article class="prose max-w-prose p-8">
         <h1>{title}</h1>
+        {frontmatter.published ? <p class="text-slate-500">Published: {frontmatter.published}</p> : null}
         <Slot />
       </article>
     </>
