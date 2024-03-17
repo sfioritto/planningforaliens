4819ce9aa88f2f4efcb743d9eab6cf7882ea3927
Sean Fioritto <sean@planningforaliens.com>
2024-03-17 17:43:35 -0500

    Fix blog nav links. add health wins

--- src/routes/blog/layout.tsx
+++ src/routes/blog/layout.tsx
@@ -1,13 +1,15 @@
 import { component$, Slot } from "@builder.io/qwik";
-import { useDocumentHead } from "@builder.io/qwik-city";
+import { useDocumentHead, useLocation } from "@builder.io/qwik-city";
 import { Date } from "~/components/Date";
-import { Link } from "~/components/Link";
+import { HomeLink, Link } from "~/components/Link";
 
 export default component$(() => {
   const { title, frontmatter } = useDocumentHead();
+  const { url } = useLocation();
+
   return (
     <>
-      <Link href="/">⃪ Home</Link>
+      {url.pathname === '/blog/' ? <HomeLink /> : <Link href="/blog/">⃪ All Articles</Link>}
       <article class="pt-8">
         <h1>{title}</h1>
         {frontmatter.published ? <p class="text-slate-500">Published: <Date>{frontmatter.published}</Date></p> : null}
