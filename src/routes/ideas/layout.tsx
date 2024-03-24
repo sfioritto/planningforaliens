9b7011db55450b7706e13329e2a816b9243ae292
Sean Fioritto <sean@planningforaliens.com>
2024-03-24 17:45:08 -0500

    Add an updated date

--- src/routes/ideas/layout.tsx
+++ src/routes/ideas/layout.tsx
@@ -1,12 +1,17 @@
 import { component$, Slot } from "@builder.io/qwik";
-import { useLocation } from "@builder.io/qwik-city";
+import { useLocation, useDocumentHead } from "@builder.io/qwik-city";
 import { Link, HomeLink } from "~/components/Link";
+import { LastUpdated } from "~/components/LastUpdated";
 
 export default component$(() => {
   const { url } = useLocation();
+  const { frontmatter } = useDocumentHead();
+
   return (
     <>
       {url.pathname === '/ideas/' ? <HomeLink /> : <Link class="block pb-8" href="/ideas/">⃪ About These Notes</Link>}
+      <LastUpdated class="text-sm pl-4" frontmatter={frontmatter} />
+
       <Slot />
     </>
   )
